4b37fabd2d6e88519ffb3af313c3679b7a925912
eunyeong-so <eunswd@gmail.com>
2021-07-24 10:09:28 +0900

    마우스 클릭 이벤트 실행

---
+++ MiniGame/Assets/Scripts/Manager/InputManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class InputManager : MonoBehaviour, IClick
+{
+    public event ClcikEventHandler ClickEvent;
+   
+    void Update()
+    {
+        ClickedBabyMonster();
+    }
+     
+    void ClickedBabyMonster()
+    { 
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray, Mathf.Infinity);
+            foreach (var hit in hits)
+            {
+                if (hit.collider)
+                {
+                    GameObject selected = hit.collider.gameObject;
+
+                    if (ClickEvent != null)
+                        ClickEvent?.Invoke(true);
+                    else
+                        Debug.Log("ClickEvent is null");
+                }
+            }
+        }
+    }
+}
