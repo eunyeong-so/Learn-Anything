a29c52bc6e72821121bda69a1a6352edce93f23e
eunyeong-so <eunswd@gmail.com>
2021-07-24 10:11:47 +0900

    몬스터 Damage, DeathEffect, InitHP, IsLive 메서드 추가

---
+++ MiniGame/Assets/Scripts/Monster/BossMonster.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMonster : Monster
+{   
+    public override void InitHP()
+    {
+        HP = 100;
+    }
+
+    public override void InitDamagePoint()
+    {
+        DamagePoint = 10;
+    }
+       
+    public override void DeathEffect(bool isTrue)
+    {
+        Bounce();
+        base.DeathEffect(isTrue);
+    } 
+
+    private void Bounce()
+    {
+        float speed = 13f;
+        float height = 1.4f;
+
+        Vector3 position = transform.position;
+
+        float newPostion = Mathf.Sin(Time.time * speed);
+
+        transform.position = new Vector3(position.x, newPostion, position.z) * height;
+    }
+}
---
+++ MiniGame/Assets/Scripts/Monster/Monster.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour 
+{
+    public int HP;
+    public int DamagePoint = 10;
+   
+    public virtual void InitHP()
+    {
+          HP = 10;
+    }
+
+    public virtual void InitDamagePoint()
+    {
+        DamagePoint = 1;
+    } 
+
+    public void Damage(bool isTrue)
+    {
+        if (isTrue)
+        {
+            HP -= DamagePoint;
+        }
+     
+    }
+
+    public bool IsLive()
+    {
+        return HP > 0;
+    }
+
+    public virtual void DeathEffect(bool isTrue)
+    { 
+        if (isTrue)
+        {
+            GetComponentInChildren<SpriteRenderer>().color = Color.red;
+            Instantiate(Resources.Load<GameObject>("Cloud"), gameObject.transform);
+
+            float delayTime = 0.5f;
+
+            Destroy(gameObject, delayTime);
+        }    
+    }
+     
+}
