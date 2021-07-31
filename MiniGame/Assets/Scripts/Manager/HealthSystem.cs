e53dc07d3ab39ddc5e2e6aee9cf8ff047b09b55e
eunyeong-so <eunswd@gmail.com>
2021-07-31 10:44:30 +0900

    HealthSystem script 추가

---
+++ MiniGame/Assets/Scripts/Manager/HealthSystem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour
+{
+    private int health = 3;
+    private int damage = 1;
+ 
+    public void healthDamage()
+    {
+        health -= damage;
+    }
+
+    public void healthCheck()
+    {
+        if (health == 0)
+        {
+            Debug.Log("No health, Game Over");
+            EditorApplication.isPlaying = false;
+        }
+           
+    }
+}
