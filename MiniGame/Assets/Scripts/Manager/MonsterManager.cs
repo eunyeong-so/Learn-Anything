8833e59c50d857ebe86054b2ba5824319829196e
eunyeong-so <eunswd@gmail.com>
2021-07-31 10:50:28 +0900

    Spawn()과 RandomPosition()을 각 몬스터 스크립트로 이동함

--- MiniGame/Assets/Scripts/Manager/MonsterManager.cs
+++ MiniGame/Assets/Scripts/Manager/MonsterManager.cs
@@ -2,75 +2,32 @@
   
 public class MonsterManager : MonoBehaviour  
 {
-    public GameObject BossMonsterParent; 
-    public GameObject BabyMonsterParent;
+    public Transform BossMonsterParent; 
+    public Transform BabyMonsterParent;
+    public Transform DevilMosnterParent;
+    private InputManager inputmanager = new InputManager();
+     
+    private BossMonster boss = new BossMonster();
+    private BabyMonster baby = new BabyMonster();
+    private DevilMonster devil = new DevilMonster();
 
-    private Monster boss;
-    private Monster baby;
-
-    public BoxCollider SpawnArea;
-
-    private IClick click;  
 
     void Start()
-    { 
-        InitBabyMonster();
-        InitBossMonster();
+    {
+        InitMonster();
+        inputmanager.ClickEvent += Damage;
+    } 
 
-        click.ClickEvent += Damage;
-
-    }
+    private void InitMonster()
+    {
+        boss.InitMonster(BossMonsterParent);
+        baby.InitMonster(BabyMonsterParent);
+        devil.InitMonster(DevilMosnterParent);
+    } 
+    
+    public void Damage()
+    {
+        Debug.Log("damage");
       
-    public void Damage(bool isTrue)
-    {    
-        boss.Damage(isTrue);
-        baby.Damage(isTrue);
-    }
-
-    private void InitBossMonster()
-    {
-        GameObject boss = Instantiate(Resources.Load<GameObject>("BossMonster"));
-         
-        boss.transform.SetParent(BossMonsterParent.transform);
-
-        this.boss = boss.GetComponent<Monster>();
-
-    }
-
-    private void InitBabyMonster()
-    { 
-        int spawnCount = 10;
-
-        for(int i = 0; i < spawnCount; i++)
-        {
-            SpawnBabyMonster();
-        }
-
-        SpawnArea.enabled = false;
-    }
-     
-    private void SpawnBabyMonster()
-    {
-        GameObject baby = Instantiate(Resources.Load<GameObject>("BabyMonster"));
-
-        this.baby = baby.GetComponent<Monster>();
-
-
-        baby.transform.SetParent(BabyMonsterParent.transform);
-
-        baby.transform.position = RandomPosition();
-    }
-
-    private Vector3 RandomPosition()
-    {
-        Vector3 basicPosition = transform.position;
-        Vector3 size = SpawnArea.size;
-
-        float posX = basicPosition.x + Random.Range(-size.x / 2f, size.x / 2f);
-        float posY = basicPosition.x + Random.Range(-size.y / 2f, size.y / 2f);
-
-        Vector3 SpawnPosition = new Vector3(posX, posY, 0);
-
-        return SpawnPosition;
     }
 }
