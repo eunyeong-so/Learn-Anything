7b7f3e81a01b9c0f33337469fb710fb46deb9ee8
eunyeong-so <eunswd@gmail.com>
2021-08-07 10:27:11 +0900

    Upload File

---
+++ MiniMonsterGame/MonsterGame/Assets/AssetStore/Character/FurBall2D_mobile/Scripts/DestroyCloud_m.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyCloud_m : MonoBehaviour {
+
+	public float DestroyTime;
+
+	void Start(){
+		Invoke("DestroyIt", DestroyTime);
+	}
+
+	void DestroyIt(){
+		Destroy(gameObject);
+	}
+}
